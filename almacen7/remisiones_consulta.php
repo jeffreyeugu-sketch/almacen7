ea4aba99eece3da98718caa1d60c084e1c718719
Jeffrey Eumaña Gutierrez <jeffreyeugu@gmail.com>
2025-11-10 14:54:06 -0600

    commit

--- almacen7/remisiones_consulta.php
+++ almacen7/remisiones_consulta.php
@@ -1,176 +1,73 @@
-<?php
-include ("mysql.php");
-
-// ===== VALIDACIÓN CRÍTICA =====
-if (!isset($_POST["NoReg"]) || empty($_POST["NoReg"]) || !is_numeric($_POST["NoReg"])) {
-    echo "<div class='alert alert-danger'>Error: No se recibió un número de registro válido.</div>";
-    exit;
-}
-
-$NoReg = intval($_POST["NoReg"]); // Asegurar que es entero
-?>
-<div class="row">
-  <div class="col">
-    <div class="card">
-      <div class="card-body"><b>Consulta de remisiones..</b>
-        <form>
-          <table class="table table-bordered table-form">                    
-            <tbody><?php 
-              include ("../mysql.php");
-              
-              // Primera consulta - SEGURA
-              $sql = "select * from _remisiones where NoReg=" . $NoReg;
-              $rsp = odbc_exec($conn,$sql);
-              
-              if (!$rsp) {
-                  die("Error en consulta: " . odbc_errormsg($conn));
-              }
-              
-              if (!odbc_fetch_row($rsp)) {
-                  die("No se encontró la remisión con NoReg: " . $NoReg);
-              }
-              ?>
-                      <tr>
-                        <td><label for="lastNameTableForm">No de remisión</label></td>
-                        <td><input type="text" class="form-control" id="Remision" placeholder="Capture el número de remisión."
-                        value='<?php echo odbc_result($rsp,"Remision") ?>'
-                        onFocus="JavaScript:FocoToma(this)" 
-                        onBlur="JavaScript:FocoPierde(this)"					
-                        ></td>
-                      </tr>
-                      <tr>
-                        <td><label for="lastNameTableForm">Fecha de Emitido</label></td>
-                        <td><input type="text" class="form-control datepicker"  id="Fecha" placeholder="Fecha de emision"
-                        value='<?php echo odbc_result($rsp,"Emitido") ?>'
-                        onFocus="JavaScript:FocoToma(this)" 
-                        onBlur="JavaScript:FocoPierde(this)"					
-                        ></td>
-                      </tr>
-                      <tr>
-                        <td><label for="lastNameTableForm">Fecha de Envío</label></td>
-                        <td><input type="text" class="form-control datepicker"  id="Fecha" placeholder="Fecha de envio"
-                        value='<?php echo odbc_result($rsp,"Envio") . ' ' . odbc_result($rsp,"Envioa_Hora") ?>'
-                        onFocus="JavaScript:FocoToma(this)" 
-                        onBlur="JavaScript:FocoPierde(this)"					
-                        ></td>
-                      </tr>
-                      <tr>
-                        <td><label for="lastNameTableForm">Mes de Entrega</label></td>
-                        <td><input type="text" class="form-control datepicker"  id="Fecha" placeholder="Mes de entrega"
-                        value='<?php echo odbc_result($rsp,"Entrega") . ' ' . odbc_result($rsp,"Entrega_Anio") ?>'
-                        onFocus="JavaScript:FocoToma(this)" 
-                        onBlur="JavaScript:FocoPierde(this)"					
-                        ></td>
-                      </tr>
-                      <tr>
-                        <td><label for="addressTableForm">Observación</label></td>
-                        <td><textarea class="form-control" id="Obs" placeholder="Escriba una observacion o comentario" rows="2"
-                        onFocus="JavaScript:FocoToma(this)" 
-                        onBlur="JavaScript:FocoPierde(this)"					
-                       ></textarea></td>
-                      </tr>
-                    </tbody>
-                  </table>                  
-                </form>
-              </div>
-            </div>
-        </div>
-        <div class="col-3 d-none d-xl-block">
-          <div class="nav-section">
-            Ayuda
-          </div>
-        </div>
-      </div>
-      <br>
       <div class="row">
         <div class="col">
             <div class="card">
-              <div class="card-body">Lista de los insumos
-                <table class="table table-striped table-bordered table-sm dt-responsive nowrap w-100" >
-                <tbody>
-                  <tr>
-                    <td width=140px>
-                      <b>C&oacute;digo</b>
-                    </td>
-                    <td width=*>
-                      <b>Nombre</b>
-                    </td>
-                    <td width=100 aling=Center><div id=ClaveStatus2 valign=Center>
-                      <b>Lote</b>
-                    </td>
-                    <td width=100 aling=Center><div id=ClaveStatus2 valign=Center>
-                      <b>Caducidad</b>
-                    </td>
-                    <td width=80 aling=Center><div id=ClaveStatus2 valign=Center>
-                      <b>Cantidad</b>
-                    </td>
-                    <td width=80 aling=Center><div id=ClaveStatus2 valign=Center>
-                      <b>Recibido</b>
-                    </td>
-                    <td width=50 aling=Center><div id=ClaveStatus2 valign=Center>
-                      &nbsp;
-                    </td>
-                  </tr><script language=javascript>NumIns = 0</script><input type=hidden name=Clave>
-                  <?php
-                  $sql = "";
-                  $sql.= "select r.* , i.Clave, i.Nombre ";
-                  $sql.= "from _remisiones_detalle r ";
-                  $sql.= "left join cat_dev_insumos i ON r.RegIns = i.NoReg ";
-                  $sql.= "where RegRem=" . $NoReg;
-                  $rsp = odbc_exec($conn,$sql);
-                  $i=0;
-                  while (odbc_fetch_row($rsp))
-                  {
-                    $i++;
-                    ?>                  
-                    <tr>
-                      <td width=140px><?php echo odbc_result($rsp,"Clave") ?>
-                      </td>
-                      <td width=*>
-                          <div id=ClaveStatus ><?php echo odbc_result($rsp,"Nombre") ?><Enter></div>
-                      </td>
-                      <td width=100 aling=Center><div id=ClaveStatus2 valign=Center>
-                        <?php echo odbc_result($rsp,"Lote") ?>
-                      </td>
-                      <td width=100 aling=Center><div id=ClaveStatus2 valign=Center>
-                        <?php echo odbc_result($rsp,"Caducidad") ?>
-                      </td>
-                      <td width=80  aling=Center><div id=ClaveStatus2 valign=Center>
-                        <input type="text" class="form-control datepicker"  id="Cantidad_<?php echo $i ?>" placeholder="Fecha de envio"
-                        value='<?php echo odbc_result($rsp,"Cantidad") ?>'
-                        onFocus="JavaScript:FocoToma(this)" 
-                        onBlur="JavaScript:FocoPierde(this)" disabled					
-                        >
-                      </td>
-                      <td width=80  aling=Center><div id=ClaveStatus2 valign=Center><input type=hidden id="NoReg_<?php echo $i ?>"
-                        value='<?php echo odbc_result($rsp,"NoReg") ?>'
-                        >
-                        <input type="text" class="form-control datepicker" id="Recibido_<?php echo $i ?>" placeholder="Fecha de envio"
-                        value='<?php echo odbc_result($rsp,"Recibido") ?>'
-                        onFocus="JavaScript:FocoToma(this)" 
-                        onBlur="JavaScript:FocoPierde(this)" disabled					
-                        >
-                      </td>
-                      <td width=50 aling=Center><div id=ClaveStatus2 valign=Center>
-                        &nbsp;
-                      </td>
-                    </tr>
-                    <?php 
-                  }
-                  ?>
-                </tbody>
-                </table>
-                <div id='Insumos123'>
-                  <button class="btn btn-secondary" type="reset"  id="btnDatosSistemaModifica" onClick="Datos_Sistema_Modifica()" disabled>Modificar</button>
-                  <button class="btn btn-success"   type="button" id="btnDatosSistemaGuardar"  onClick="Registrar_Entrada(<?php echo $i ?>)" >Registrar Entrada</button>
-                  <button class="btn btn-success"   type="button" id="btnDatosSistemaRegistra" onClick="Registrar_Guardar(<?php echo $i . "," . $NoReg ?>)" hidden>Guardar Entrada</button>
-                </div>
+              <div class="card-body">
+                <form>
+                  <table class="table table-bordered table-form">
+                    <tbody>
+                      <?php
+                      include ("../mysql.php");
+                      $sql = "";
+                      $sql = $sql . "select * from sis_info";  
+                      $rs  = odbc_exec($conn,$sql);
+                      $txtDomicilio = odbc_result($rs,"Domicilio");
+                      ?>
+                      <tr>
+                        <td><label for="firstNameTableForm">Nombre</label></td>
+                        <td><input type="text" value='<?php echo odbc_result($rs,"Nombre") ?>' class="form-control" id="Nombre" placeholder="Escriba el nombre comercial"
+                        readonly
+                        ></td>
+                      </tr>
+                      <tr>
+                        <td><label for="lastNameTableForm">Institución</label></td>
+                        <td><input type="text" value='<?php echo odbc_result($rs,"RazonSocial") ?>' class="form-control" id="RazonSocial" placeholder="Escriba la razón social o el nombre de la Institución"
+                        readonly
+                        ></td>
+                      </tr>
+                      <tr>
+                        <td><label for="emailTableForm">Email</label></td>
+                        <td><input type="email" class="form-control" id="email" placeholder="Escriba el correo electrónico"
+                        readonly
+                        ></td>
+                      </tr>
+                      <tr>
+                        <td><label for="addressTableForm">Dirección</label></td>
+                        <td><textarea class="form-control" id="Domicilio" placeholder="Escriba la dirección" rows="2"
+                        readonly
+                        ><?php echo str_replace('\n',chr(13), $txtDomicilio) ?></textarea></td>
+                      </tr>
+                      <!-- tr>
+                        <td><label for="languageTableForm">Language</label></td>
+                        <td>
+                          <select class="custom-select" id="languageTableForm">
+                            <option selected="">English</option>
+                            <option value="1">Spanish</option>
+                            <option value="2">Chinese</option>
+                          </select>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td><label for="avatarTableForm">Profil Picture</label></td>
+                        <td>
+                          <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="avatarTableForm">
+                            <label class="custom-file-label" for="avatarTableForm">Choose file</label>
+                          </div>
+                        </td>
+                      </tr -->
+                    </tbody>
+                  </table>
+                  <button class="btn btn-secondary" type="reset" onClick="Datos_Sistema_Modifica()">Modificar</button>
+                  <button class="btn btn-success" type="button">Guardar</button>
+                </form>
               </div>
             </div>
+
         </div>
+
         <div class="col-3 d-none d-xl-block">
           <div class="nav-section">
-          
+            Ayuda
           </div>
         </div>
       </div>
--- funciones.007.js
+++ funciones.007.js
@@ -131,3 +131,397 @@ function cerrarSession(event)
     onClose(event);    
 }
 window.addEventListener("load", Init, false);
+
+// ============================================================================
+// MÓDULO DE REMISIONES - CÓDIGO MODERNIZADO
+// ============================================================================
+// Este código moderniza las funciones de remisiones del sistema
+// Fecha de implementación: [AGREGAR FECHA]
+// Reemplaza: XMLHttpRequest por Fetch API, alert() por notificaciones Toast
+// ============================================================================
+
+// ==================== CONFIGURACIÓN GLOBAL ====================
+const RemisionesConfig = {
+    baseUrl: (() => {
+      const url = window.location.href;
+      const lastSlash = url.lastIndexOf('/');
+      return url.substring(0, lastSlash);
+    })(),
+    
+    endpoints: {
+      remisionesGuarda: '/almacen7/remisiones_guarda.php',
+      remisionesConsulta: '/almacen7/remisiones_consulta.php',
+      remisionesEntrada: '/almacen7/remisiones_entrada.php',
+      entradasGuarda: '/almacen7/entradas_guarda.php'
+    }
+  };
+  
+  // ==================== UTILIDADES ====================
+  const RemisionesUtils = {
+    showSuccess(message) {
+      this.showToast(message, 'success');
+    },
+  
+    showError(message) {
+      this.showToast(message, 'danger');
+    },
+  
+    showInfo(message) {
+      this.showToast(message, 'info');
+    },
+  
+    showToast(message, type = 'info') {
+      // Si existe Toastr
+      if (typeof toastr !== 'undefined') {
+        toastr[type](message);
+        return;
+      }
+  
+      // Fallback con Bootstrap Toast
+      let toastContainer = document.getElementById('toastContainer');
+      if (!toastContainer) {
+        toastContainer = document.createElement('div');
+        toastContainer.id = 'toastContainer';
+        toastContainer.className = 'toast-container position-fixed top-0 end-0 p-3';
+        toastContainer.style.zIndex = '9999';
+        document.body.appendChild(toastContainer);
+      }
+  
+      const toastId = 'toast-' + Date.now();
+      const bgClass = 'bg-' + type;
+      
+      const toastHTML = `
+        <div id="${toastId}" class="toast align-items-center text-white ${bgClass} border-0" role="alert">
+          <div class="d-flex">
+            <div class="toast-body">${message}</div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+          </div>
+        </div>
+      `;
+  
+      toastContainer.insertAdjacentHTML('beforeend', toastHTML);
+      
+      const toastElement = document.getElementById(toastId);
+      const toast = new bootstrap.Toast(toastElement, { delay: 3000 });
+      toast.show();
+  
+      toastElement.addEventListener('hidden.bs.toast', () => {
+        toastElement.remove();
+      });
+    },
+  
+    validateRequired(value, fieldName) {
+      if (!value || value.trim() === '') {
+        this.showError(`El campo ${fieldName} es requerido`);
+        return false;
+      }
+      return true;
+    },
+  
+    toggleButton(buttonId, disabled, text = null) {
+      const button = document.getElementById(buttonId);
+      if (button) {
+        button.disabled = disabled;
+        if (text) button.textContent = text;
+      }
+    },
+  
+    toggleField(fieldId, disabled) {
+      const field = document.getElementById(fieldId);
+      if (field) {
+        field.disabled = disabled;
+        if (disabled) {
+          field.classList.add('bg-light');
+        } else {
+          field.classList.remove('bg-light');
+        }
+      }
+    }
+  };
+  
+  // ==================== API DE REMISIONES ====================
+  const RemisionesAPI = {
+    async post(endpoint, data) {
+      try {
+        const formData = new URLSearchParams();
+        for (const key in data) {
+          formData.append(key, data[key]);
+        }
+  
+        const response = await fetch(RemisionesConfig.baseUrl + endpoint, {
+          method: 'POST',
+          headers: {
+            'Accept': '*/*',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: formData.toString()
+        });
+  
+        if (!response.ok) {
+          throw new Error(`Error HTTP: ${response.status}`);
+        }
+  
+        return await response.text();
+      } catch (error) {
+        console.error('Error en petición:', error);
+        throw error;
+      }
+    },
+  
+    async guardarRemision(data) {
+      return await this.post(RemisionesConfig.endpoints.remisionesGuarda, data);
+    },
+  
+    async consultarRemision(noReg) {
+      return await this.post(RemisionesConfig.endpoints.remisionesConsulta, { NoReg: noReg });
+    },
+  
+    async registrarEntrada(data) {
+      return await this.post(RemisionesConfig.endpoints.remisionesEntrada, data);
+    },
+  
+    async guardarEntradas(data) {
+      return await this.post(RemisionesConfig.endpoints.entradasGuarda, data);
+    }
+  };
+  
+  // ==================== CONTROLADOR DE REMISIONES ====================
+  const RemisionesController = {
+    async guardar(noReg) {
+      try {
+        const remision = document.getElementById('Remision').value;
+        const idProveedor = document.getElementById('IdProveedor').value;
+        const fecha = document.getElementById('Fecha').value;
+        const obs = document.getElementById('Obs').value;
+  
+        if (!RemisionesUtils.validateRequired(remision, 'Número de Remisión')) return;
+        if (!RemisionesUtils.validateRequired(idProveedor, 'Proveedor')) return;
+        if (!RemisionesUtils.validateRequired(fecha, 'Fecha')) return;
+  
+        RemisionesUtils.toggleField('Remision', true);
+        RemisionesUtils.toggleField('IdProveedor', true);
+        RemisionesUtils.toggleField('Fecha', true);
+        RemisionesUtils.toggleField('Obs', true);
+        RemisionesUtils.toggleButton('btnDatosGuardar', true, 'Guardando...');
+  
+        const data = {
+          Remision: remision,
+          IdProveedor: idProveedor,
+          Fecha: fecha,
+          Obs: obs
+        };
+  
+        const resultado = await RemisionesAPI.guardarRemision(data);
+  
+        if (resultado.includes('1') || resultado.includes('success')) {
+          RemisionesUtils.showSuccess('✓ Remisión guardada exitosamente');
+          RemisionesUtils.toggleButton('btnDatosModifica', false);
+        } else {
+          RemisionesUtils.showError('Error al guardar la remisión: ' + resultado);
+          RemisionesUtils.toggleField('Remision', false);
+          RemisionesUtils.toggleField('IdProveedor', false);
+          RemisionesUtils.toggleField('Fecha', false);
+          RemisionesUtils.toggleField('Obs', false);
+        }
+  
+      } catch (error) {
+        RemisionesUtils.showError('Error de conexión: ' + error.message);
+        console.error('Error al guardar remisión:', error);
+        
+        RemisionesUtils.toggleField('Remision', false);
+        RemisionesUtils.toggleField('IdProveedor', false);
+        RemisionesUtils.toggleField('Fecha', false);
+        RemisionesUtils.toggleField('Obs', false);
+      } finally {
+        RemisionesUtils.toggleButton('btnDatosGuardar', false, 'Guardar');
+      }
+    },
+  
+    modificar() {
+      RemisionesUtils.toggleField('Remision', false);
+      RemisionesUtils.toggleField('IdProveedor', false);
+      RemisionesUtils.toggleField('Fecha', false);
+      RemisionesUtils.toggleField('Obs', false);
+      RemisionesUtils.toggleButton('btnDatosModifica', true);
+      RemisionesUtils.toggleButton('btnDatosGuardar', false);
+      RemisionesUtils.showInfo('Puede modificar los campos ahora');
+    },
+  
+    async consultar(noReg) {
+      try {
+        if (!noReg || isNaN(noReg)) {
+          RemisionesUtils.showError('Número de registro inválido');
+          return;
+        }
+  
+        const html = await RemisionesAPI.consultarRemision(noReg);
+        document.getElementById('Pagina').innerHTML = html;
+        RemisionesUtils.showSuccess('Remisión consultada');
+  
+      } catch (error) {
+        RemisionesUtils.showError('Error al consultar remisión: ' + error.message);
+        console.error('Error:', error);
+      }
+    }
+  };
+  
+  // ==================== CONTROLADOR DE ENTRADAS ====================
+  const EntradasController = {
+    registrarEntrada(numIns) {
+      RemisionesUtils.toggleButton('btnDatosSistemaGuardar', true);
+      
+      for (let i = 1; i <= numIns; i++) {
+        const cantidad = document.getElementById(`Cantidad_${i}`).value;
+        const recibidoField = document.getElementById(`Recibido_${i}`);
+        
+        if (recibidoField) {
+          recibidoField.disabled = false;
+          recibidoField.value = cantidad;
+          recibidoField.classList.remove('bg-light');
+        }
+      }
+      
+      const btnRegistrar = document.getElementById('btnDatosSistemaRegistra');
+      if (btnRegistrar) {
+        btnRegistrar.hidden = false;
+      }
+      
+      RemisionesUtils.showInfo('Ajuste las cantidades recibidas si es necesario');
+    },
+  
+    async guardar(numIns, noReg) {
+      try {
+        const btnRegistrar = document.getElementById('btnDatosSistemaRegistra');
+        if (btnRegistrar) {
+          btnRegistrar.hidden = true;
+        }
+  
+        let errores = 0;
+        let exitosos = 0;
+  
+        for (let i = 1; i <= numIns; i++) {
+          const recibidoField = document.getElementById(`Recibido_${i}`);
+          const noRegField = document.getElementById(`NoReg_${i}`);
+          
+          if (!recibidoField || !noRegField) continue;
+  
+          const cantidad = recibidoField.value;
+          const noReg2 = noRegField.value;
+  
+          try {
+            await this.registrarInsumo(noReg, noReg2, cantidad);
+            recibidoField.disabled = true;
+            recibidoField.classList.add('bg-light');
+            exitosos++;
+          } catch (error) {
+            errores++;
+            console.error(`Error en insumo ${i}:`, error);
+          }
+        }
+  
+        if (errores === 0) {
+          RemisionesUtils.showSuccess(`✓ Se registraron ${exitosos} insumos correctamente`);
+        } else {
+          RemisionesUtils.showError(`Se completaron ${exitosos} insumos, ${errores} con errores`);
+        }
+  
+      } catch (error) {
+        RemisionesUtils.showError('Error al guardar entradas: ' + error.message);
+        console.error('Error:', error);
+      }
+    },
+  
+    async registrarInsumo(noReg, noReg2, cantidad) {
+      const data = {
+        NoRegRem: noReg,
+        NoReg: noReg2,
+        Cantidad: cantidad
+      };
+  
+      const resultado = await RemisionesAPI.registrarEntrada(data);
+      
+      if (resultado.includes('Error') || resultado.includes('error')) {
+        throw new Error(resultado);
+      }
+      
+      return resultado;
+    },
+  
+    async guardarRemision(claves, lotes, fechas, cantidades) {
+      try {
+        const data = {
+          Claves: claves,
+          Lotes: lotes,
+          Fechas: fechas,
+          Cantidad: cantidades
+        };
+  
+        console.log('Guardando remisión:', data);
+        
+        const resultado = await RemisionesAPI.guardarEntradas(data);
+        
+        if (resultado.includes('Error')) {
+          RemisionesUtils.showError('Error al guardar: ' + resultado);
+        } else {
+          RemisionesUtils.showSuccess('✓ Remisión guardada correctamente');
+        }
+  
+      } catch (error) {
+        RemisionesUtils.showError('Error de conexión: ' + error.message);
+        console.error('Error:', error);
+      }
+    }
+  };
+  
+  // ==================== FUNCIONES GLOBALES (COMPATIBILIDAD CON HTML EXISTENTE) ====================
+  // Estas funciones mantienen los mismos nombres que el código antiguo para compatibilidad
+  
+  function Cat_Dev_Remisiones_Guardar(noReg) {
+    RemisionesController.guardar(noReg);
+  }
+  
+  function Cat_dev_Remisiones_Modifica() {
+    RemisionesController.modificar();
+  }
+  
+  function RemisionConsulta(noReg) {
+    RemisionesController.consultar(noReg);
+  }
+  
+  function Registrar_Entrada(numIns) {
+    EntradasController.registrarEntrada(numIns);
+  }
+  
+  function Registrar_Guardar(numIns, noReg) {
+    EntradasController.guardar(numIns, noReg);
+  }
+  
+  function RemisionGuarda(claves, lotes, fechas, cantidad) {
+    EntradasController.guardarRemision(claves, lotes, fechas, cantidad);
+  }
+  
+  // ==================== INICIALIZACIÓN ====================
+  // Solo se ejecuta si el DOM ya está cargado
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', inicializarModuloRemisiones);
+  } else {
+    inicializarModuloRemisiones();
+  }
+  
+  function inicializarModuloRemisiones() {
+    console.log('✓ Módulo de Remisiones modernizado cargado correctamente');
+    
+    // Configurar Toastr si está disponible
+    if (typeof toastr !== 'undefined') {
+      toastr.options = {
+        closeButton: true,
+        progressBar: true,
+        positionClass: 'toast-top-right',
+        timeOut: 3000
+      };
+    }
+  }
+  
+  // ============================================================================
+  // FIN DEL MÓDULO DE REMISIONES
+  // ============================================================================
